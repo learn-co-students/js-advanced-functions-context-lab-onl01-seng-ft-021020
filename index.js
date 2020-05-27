47d62d2835cf610c784ea2fe26e675dce64e244a
github username <github email address>
2020-05-27 16:03:35 -0700

    Done.

--- index.js
+++ index.js
@@ -9,6 +9,58 @@
  for you to use if you need it!
  */
 
+function createEmployeeRecord(infoArray) {
+    const employeeObj = {
+        firstName: infoArray[0],
+        familyName: infoArray[1],
+        title: infoArray[2],
+        payPerHour: infoArray[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    };
+
+    return employeeObj;
+}
+
+function createEmployeeRecords(employeeArray) {
+    return employeeArray.map(employee => {
+        return createEmployeeRecord(employee);
+    });
+}
+
+function createTimeInEvent(dateStamp) {
+    const timeObject = {
+        type: "TimeIn",
+        hour: parseInt(dateStamp.split(" ")[1]),
+        date: dateStamp.split(" ")[0]
+    }
+
+    this.timeInEvents.push(timeObject);
+    return this
+}
+
+function createTimeOutEvent(dateStamp) {
+    const timeObject = {
+        type: "TimeOut",
+        hour: parseInt(dateStamp.split(" ")[1]),
+        date: dateStamp.split(" ")[0]
+    }
+
+    this.timeOutEvents.push(timeObject);
+    return this
+}
+
+function hoursWorkedOnDate(dateString) {
+    const timeOut = this.timeOutEvents.find(event => event.date === dateString).hour
+    const timeIn = this.timeInEvents.find(event => event.date === dateString).hour
+
+    return (timeOut - timeIn) / 100
+}
+
+function wagesEarnedOnDate(dateString) {
+    return hoursWorkedOnDate.call(this, dateString) * this.payPerHour
+}
+
 let allWagesFor = function () {
     let eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
@@ -19,4 +71,15 @@ let allWagesFor = function () {
     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
     return payable
+}
+
+function findEmployeeByFirstName(srcArray, firstName) {
+    return srcArray.find(employee => employee.firstName === firstName)
+}
+
+function calculatePayroll(employeeArray) {
+    const allPay = employeeArray.map(employee => {
+        return allWagesFor.call(employee)
+    })
+    return allPay.reduce((total, num) => total + num)
 }
